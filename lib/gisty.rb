17c4f9f603d580d2a41861214a5024dfa55450af
Michael Rose <elementation@gmail.com>
2013-09-08 21:12:40 -0600

    Working

--- lib/gisty.rb
+++ lib/gisty.rb
@@ -1,13 +1,10 @@
 require "gisty/version"
+require "gisty/github_api"
 
 module Gisty
   require "highline/import"
 
-  ## CONFIG ##
-  API_KEY = ""
-  @api = GithubApi.new API_KEY
-
-  def print_gist_header(gist)
+  def self.print_gist_header(gist)
     puts "#{"#"*72}"
     puts "Files: " + gist['files'].map { |k, v| k }.join(',')
     puts "Languages: " + gist['files'].map { |k, v| v['language'] }.join(',')
@@ -22,7 +19,11 @@ module Gisty
     puts "-"*72
   end
 
-  def agree( yes_or_no_question, character = nil )
+  def self.print_file_contents(api, gist)
+    puts api.get_gist((gist['files'].first).last["raw_url"])
+  end
+
+  def self.agree( yes_or_no_question, character = nil )
     ask(yes_or_no_question, lambda { |yn| yn.downcase[0] }) do |q|
       q.responses[:not_valid]    = 'Please enter "yes", "no", or "quit".'
       q.responses[:ask_on_error] = :question
@@ -32,12 +33,9 @@ module Gisty
     end
   end
 
-  def print_file_contents(api, gist)
-    puts api.get_gist((gist['files'].first).last["raw_url"])
-  end
+  def self.check_gists(api_key)
+    api = GithubApi.new api_key
 
-
-  def check_gists()
     api.with_gists do |gist|
       print_gist_header(gist)
       print_file_contents(api, gist)
